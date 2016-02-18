10dc0cd65fb5a5d4487311036c3eefeb704b8c66
DempseyS <Dempsey@Dempseys-MacBook-Air.local>
2016-02-17 16:05:00 -0800

    added comments

--- Yas.java
+++ Yas.java
@@ -4,12 +4,12 @@ import javax.swing.*;
 import java.awt.event.*;
 import java.applet.*;
 
-public class Yas
+public class Yas //finds median of ArrayList
 {
     // instance variables - replace the example below with your own
 
     public static void main(String [] args){
-        Scanner reader = new Scanner (System.in); 
+        Scanner reader = new Scanner (System.in);
         Random rand = new Random();
 
         ArrayList <Integer> nums = new ArrayList();
@@ -23,7 +23,7 @@ public class Yas
             System.out.print("" + nums.get(i)+",");
             System.out.println("median = " + median(nums));
     }
-    
+
     public static int min(ArrayList <Integer> nums){
         int g = nums.get(0);
         for (int i = 0; i < nums.size(); i++)
@@ -32,7 +32,7 @@ public class Yas
 
         return g;
     }
-    
+
     public static int max(ArrayList <Integer> nums){
         int g = nums.get(0);
         for (int i = 0; i < nums.size(); i++)
@@ -41,28 +41,28 @@ public class Yas
 
         return g;
     }
-    
-    
+
+
     public static double median(ArrayList <Integer> nums){
-        
+
         int x = max(nums);
         int y = min(nums);
-        
+
         double a = ((double)x-y)/2;
         return y+a;
-        
-        
-      
-    
-    
+
+
+
+
+
     }
-    
-    
-    
-    
-    
-    
-    
-    
-    
+
+
+
+
+
+
+
+
+
 }
